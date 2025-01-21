3102cc54927b2f1ee4706d34df6bd87b9433d965
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-21 19:20:04 +0700

    Add custom colors to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				green: "#017d7d",
+				black: "#1b1b1b",
+				gray: "#a6a6a6",
+			},
+		},
 	},
 	plugins: [],
 }
