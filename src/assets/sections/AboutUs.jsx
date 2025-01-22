e6347bd39e66d6e3cf23934cfe8b7aa96eab4d3f
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-23 02:10:13 +0700

    Enhance AboutUs section with image overlay and experience card

--- src/assets/sections/AboutUs.jsx
+++ src/assets/sections/AboutUs.jsx
@@ -9,16 +9,18 @@ import "swiper/swiper-bundle.css"
 export default function AboutUs() {
 	return (
 		<Section id={"about-us"}>
-			<Image src={"./src/assets/img/about/1.jpg"} alt={"About Us"} />
+			<div className="relative">
+				<Image src={"./src/assets/img/about/1.jpg"} className={"aspect-square"} alt={"About Us"} />
+				<Card className={"absolute bottom-2 left-2 w-[80%] rounded-lg"}>
+					<h5 className="text-xl font-medium">30 Years of experience</h5>
+					<p>We have been serving the comunity eith excellence and compassion for 30 years</p>
+				</Card>
+			</div>
 
 			<SectionTitle title={"About Us"} align={"left"} headline={"We Provide Quality Care for Your Health"} description={"We are committed to delivering exceptional healthcare services to prioritize your well-being with our experienced professionals strive to create an environment that fosters healing and promotes a healthy lifestyle."} />
 
 			<div className="overflow-hidden w-full">
-				<Swiper
-                    loop={false}
-                    slidesPerView={1.7}
-                    spaceBetween={20}
-				>
+				<Swiper loop={false} slidesPerView={1.7} spaceBetween={20}>
 					{AboutData.map((about, index) => (
 						<SwiperSlide key={index} className="w-52">
 							<Card className="text-center">
