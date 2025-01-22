d8d45388ab68139c584c3deb8d7259f9544e076f
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-22 22:12:24 +0700

    Add lightgreen color and aspect ratio utility to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
 				green: "#017d7d",
 				black: "#1b1b1b",
 				gray: "#a6a6a6",
-			},
+				lightgreen: "#e8f1fd",
+            },
+            aspectRatio: {
+                '16/10': 16 / 10, 
+            }
 		},
 	},
 	plugins: [],
