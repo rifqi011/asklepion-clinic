5dbb27b2a7c49abf422fe4250d1ff7ee33333f7e
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-23 01:09:46 +0700

    Add AboutUs section with Swiper component and related data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./assets/components/Header"
+import AboutUs from "./assets/sections/AboutUs"
 import Home from "./assets/sections/Home"
 
 export default function App() {
 	return (
 		<>
-            <Header />
-            
-            <Home />
+			<Header />
+
+			<Home />
+
+			<AboutUs />
 		</>
 	)
 }
---
+++ src/assets/sections/AboutUs.jsx
@@ -0,0 +1,34 @@
+import Section from "../components/Section"
+import SectionTitle from "../components/SectionTitle"
+import Image from "../components/Image"
+import AboutData from "../data/about-data.json"
+import Card from "../components/Card"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/swiper-bundle.css"
+
+export default function AboutUs() {
+	return (
+		<Section id={"about-us"}>
+			<Image src={"./src/assets/img/about/1.jpg"} alt={"About Us"} />
+
+			<SectionTitle title={"About Us"} align={"left"} headline={"We Provide Quality Care for Your Health"} description={"We are committed to delivering exceptional healthcare services to prioritize your well-being with our experienced professionals strive to create an environment that fosters healing and promotes a healthy lifestyle."} />
+
+			<div className="overflow-hidden w-full">
+				<Swiper
+                    loop={false}
+                    slidesPerView={1.7}
+                    spaceBetween={20}
+				>
+					{AboutData.map((about, index) => (
+						<SwiperSlide key={index} className="w-52">
+							<Card className="text-center">
+								<h4 className="text-4xl text-green">{about.value}</h4>
+								<p>{about.text}</p>
+							</Card>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		</Section>
+	)
+}
