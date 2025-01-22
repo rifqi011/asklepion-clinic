445bb0cf688c63851b9577ec0d444fecf3027ca6
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-23 02:10:07 +0700

    Add object-cover class to Image component for better image scaling

--- src/assets/components/Image.jsx
+++ src/assets/components/Image.jsx
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Image({ src, alt, className }) {
-	return <img src={src} alt={alt} className={`rounded-2xl ${className}`} />
+	return <img src={src} alt={alt} className={`rounded-2xl object-cover ${className}`} />
 }
