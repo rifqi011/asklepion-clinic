a3e9c64c9a78974fc893814dcc1a095448c2a912
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-24 19:49:28 +0700

    Refactor SectionTitle component structure for improved layout and readability

--- src/assets/components/SectionTitle.jsx
+++ src/assets/components/SectionTitle.jsx
@@ -1,13 +1,13 @@
 export default function SectionTitle({ title, headline, description, children, className, align }) {
 	return (
-		<>
-            <div className={`flex flex-col gap-6 ${className} ${align === "center" ? "text-center" : "text-left"}`}>
-                <h2 className={`text-green text-lg bg-lightgreen font-semibold px-3 py-1 rounded-lg ${align === "center" ? "self-center" : "self-start"}`}>{title}</h2>
+		<div className={`flex flex-col gap-6  ${className} ${align === "center" ? "text-center" : "text-left"}`}>
+			<div className={`flex flex-col gap-6`}>
+				<h2 className={`text-green text-lg bg-lightgreen font-semibold px-3 py-1 rounded-lg ${align === "center" ? "self-center" : "self-start"}`}>{title}</h2>
 				<h3 className="text-5xl font-medium">{headline}</h3>
 				<p className="text-lg">{description}</p>
-            </div>
-            
+			</div>
+
 			{children}
-		</>
+		</div>
 	)
 }
