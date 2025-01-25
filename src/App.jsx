6bff2d0296eb8c7ae33e1f878c3fecf3695cb4c9
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-25 11:54:36 +0700

    Add Quality section with data integration and update App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import AboutUs from "./sections/AboutUs"
 import Doctors from "./sections/Doctors"
 import Home from "./sections/Home"
+import Quality from "./sections/Quality"
 import WhyChooseUs from "./sections/WhyChooseUs"
 
 export default function App() {
@@ -11,11 +12,13 @@ export default function App() {
 
 			<Home />
 
-            <AboutUs />
-            
-            <WhyChooseUs />
+			<AboutUs />
 
-            <Doctors />
+			<WhyChooseUs />
+
+			<Doctors />
+
+			<Quality />
 		</>
 	)
 }
---
+++ src/sections/Quality.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Section from "../components/Section"
+import SectionTitle from "../components/SectionTitle"
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons"
+import ButtonPrimary from "../components/ButtonPrimary"
+import QualityData from "../data/quality.json"
+import Image from "../components/Image"
+
+export default function Quality() {
+	return (
+		<Section id={"quality"}>
+            <SectionTitle title={"Quality"} headline={"Our Performance Quality Care, Made for You"} description={"We understand that each individual has distinct requirements when it comes to their health and well-being."}>
+                <ul className="flex flex-col gap-4">
+                    {QualityData.map((quality, index) => (
+                        <li key={index} className="flex gap-2 items-center text-xl">
+                            <FontAwesomeIcon icon={faCheckCircle} className="text-green" />
+                            <p>{quality.text}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <ButtonPrimary href={"/"} className={"self-start px-8 mt-2"}>Learn More</ButtonPrimary>
+            </SectionTitle>
+
+            <Image src={"./src/assets/img/quality/1.jpg"} alt={"Quality"} className={"aspect-square"} />
+		</Section>
+	)
+}
