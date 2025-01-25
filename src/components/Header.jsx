cf7f3a72fea28ef214af7c6409cd1b2a96f708e5
rifqi011 <rifqibanusafingi11@gmail.com>
2025-01-25 14:39:30 +0700

    Enhance Header component with scroll animation and update CSS for responsive layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import HamburgerIcon from "./HamburgerIcon"
 import Logo from "./Logo"
 
 export default function Header() {
+    const [scrolled, setScrolled] = useState(false)
+    const [hasAnimated, setHasAnimated] = useState(false)
+
+    // scroll
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                if (!hasAnimated) {
+                    setScrolled(true)
+                    setHasAnimated(true)
+                }
+
+            } else {
+                setScrolled(false)
+                setHasAnimated(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        //un-mount
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [hasAnimated])
+
 	return (
-		<header className="container left-0 top-0 fixed h-16 flex justify-between items-center bg-white z-40 shadow-lg shadow-gray">
+        <header className={`container left-0 top-0 fixed h-16 flex justify-between items-center bg-white z-40 transition-all duration-300 ${scrolled ? " shadow-lg shadow-gray" : ""}`}>
 			<Logo />
 
 			<HamburgerIcon isOpen={false} />
