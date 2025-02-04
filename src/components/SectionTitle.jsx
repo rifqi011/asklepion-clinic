55f1504e92841da28374114c78f4c2fd5cbb4bd2
rifqi011 <rifqibanusafingi11@gmail.com>
2025-02-04 15:32:27 +0700

    Add sectionId prop to SectionTitle for responsive headline sizing in Home section

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,9 +1,9 @@
-export default function SectionTitle({ title, headline, description, children, className, align }) {
+export default function SectionTitle({ title, headline, description, children, className, align, sectionId }) {
 	return (
-		<div className={`flex flex-col gap-6  ${className} ${align === "center" ? "text-center" : "text-left"}`}>
+		<div className={`flex flex-col gap-6 ${className} ${align === "center" ? "text-center" : "text-left"}`}>
 			<div className={`flex flex-col gap-6`}>
 				<h2 className={`text-green text-lg bg-lightgreen font-semibold px-3 py-1 rounded-lg ${align === "center" ? "self-center" : "self-start"}`}>{title}</h2>
-				<h3 className="text-5xl font-medium">{headline}</h3>
+				<h3 className={`font-medium text-5xl  ${sectionId === "home" ? "md:text-6xl" : " md:text-4xl"}`}>{headline}</h3>
 				<p className="text-lg">{description}</p>
 			</div>
 
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -11,12 +11,12 @@ export default function Home() {
 	return (
 		<>
 			<Section id={"home"} className={"mt-14 text-center"}>
-                <SectionTitle title={"Health Harmony"} align={"center"} headline={"Your Gateway to Optimal Health Solution"} description={"Our platform serves as your gateway to a healthier life, offering personalized guidance, valuable insights, and support for your well-being."} />
+                <SectionTitle title={"Health Harmony"} align={"center"} headline={"Your Gateway to Optimal Health Solution"} description={"Our platform serves as your gateway to a healthier life, offering personalized guidance, valuable insights, and support for your well-being."} sectionId={"home"} />
 
-				<div className="flex flex-col gap-6">
-					<Image src={"./src/assets/img/hero/1.jpg"} alt={"Hero"} className="hidden md:block" />
+				<div className="flex flex-col gap-6 md:flex-row">
+					<Image src={"./src/assets/img/hero/1.jpg"} alt={"Hero"} className="hidden lg:block lg:w-[20%]" />
 
-					<Image src={"./src/assets/img/hero/2.jpg"} alt={"Hero"} className={"aspect-16/10"} />
+					<Image src={"./src/assets/img/hero/2.jpg"} alt={"Hero"} className={"aspect-16/10 md:aspect-auto md:w-1/2"} />
 
 					<div className="flex flex-col gap-6">
 						<Image src={"./src/assets/img/hero/3.jpg"} alt={"Hero"} />
@@ -43,7 +43,7 @@ export default function Home() {
 					modules={[Autoplay]}
                 >
                     {PatnersData.map((patner, index) => (
-                        <SwiperSlide key={index}>
+                        <SwiperSlide key={index} style={{ width: "100%", display: "flex", justifyContent: "center" }}>
                             <img src={patner.logo} />
                         </SwiperSlide>
                     ))}
