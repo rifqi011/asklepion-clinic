43cbe0a85529f5355dfafe24046ff0d9e2cb14ac
rifqi011 <rifqibanusafingi11@gmail.com>
2025-02-15 19:01:47 +0700

    Enhance AboutUs section with responsive image aspect ratio and update Swiper breakpoints for improved layout

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -10,7 +10,7 @@ export default function AboutUs() {
 	return (
 		<Section id={"about-us"}>
 			<div className="relative">
-				<Image src={"./src/assets/img/about/1.jpg"} className={"aspect-square"} alt={"About Us"} />
+				<Image src={"./src/assets/img/about/1.jpg"} className={"aspect-square md:aspect-auto"} alt={"About Us"} />
 				<Card className={"absolute bottom-2 left-2 w-[80%] rounded-lg"}>
 					<h5 className="text-xl font-medium">30 Years of experience</h5>
 					<p>We have been serving the comunity eith excellence and compassion for 30 years</p>
@@ -20,7 +20,7 @@ export default function AboutUs() {
 			<SectionTitle title={"About Us"} align={"left"} headline={"We Provide Quality Care for Your Health"} description={"We are committed to delivering exceptional healthcare services to prioritize your well-being with our experienced professionals strive to create an environment that fosters healing and promotes a healthy lifestyle."} sectionId={"about-us"} />
 
 			<div className="overflow-hidden w-full">
-				<Swiper loop={false} slidesPerView={1.7} spaceBetween={20}>
+                <Swiper loop={false} slidesPerView={1.7} spaceBetween={20} breakpoints={{ 768: { slidesPerView: 3 }}}>
 					{AboutData.map((about, index) => (
 						<SwiperSlide key={index} className="w-52">
 							<Card className="text-center">
